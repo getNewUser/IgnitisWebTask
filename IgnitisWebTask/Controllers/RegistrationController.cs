a19f7a7d5e4f1eac3a999714436d66643cd12f59
VilmantasN <vilmantas.neviera@gmail.com>
2020-01-06 17:05:18 +0200

    Initial project commit

---
+++ IgnitisWebTask/Controllers/RegistrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IgnitisWebTask.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Random()
+        {
+            return View();
+        }
+    }
+}
---
+++ IgnitisWebTask/Models/RegistrationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IgnitisWebTask.Models
+{
+    public class RegistrationModel
+    {
+        public Guid Id { get; set; }
+        public bool IsRequiredContractWork { get; set; }
+        public String ContractWorkAsignee { get; set; }
+        public bool IsBusinessClient { get; set; }
+        public String CalculationMethod { get; set; }
+        public bool IsImportantClient { get; set; }
+
+    }
+}
